5df28467b26bda088450c3884729eb5398f7945b
Alejandro7Hub <jandrods7@gmail.com>
2022-06-21 19:39:33 +0200

    prueba1

---
+++ src/Principal.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner teclado= new Scanner(System.in);
+		double a;
+		double b;
+		System.out.println("Introduce el primer número ");
+		a=teclado.nextDouble();
+		System.out.println("Introduce el segundo número");
+		b=teclado.nextDouble();
+		claseObjeto objeto= new claseObjeto(a, b);
+		int respuesta=0;
+		do {
+			menu();
+			System.out.println("Escoge una opci�n");
+			respuesta=teclado.nextInt();
+			switch (respuesta) {
+			case 1:
+				objeto.suma(a, b);
+				break;
+			case 2:
+				objeto.resta(a, b);
+				break;
+			case 3:
+				objeto.multiplicacion(a, b);
+				break;
+			case 4:
+				objeto.division(a, b);
+				break;
+			case 5:
+				objeto.potenciacion(a, b);
+				break;
+			case 6:
+				objeto.raiz(a, b);
+				break;
+			case 0:
+				System.out.println("Fin Programa");
+				break;
+			default:
+				System.out.println("ERROR");
+				break;
+			}
+		} while (respuesta!=0);
+
+	}
+
+	private static void menu() {
+		System.out.println("1.suma");
+		System.out.println("2.resta");
+		System.out.println("3.multiplicacion");
+		System.out.println("4.division");
+		System.out.println("5.potenciacion");
+		System.out.println("6.Raiz");
+		System.out.println("0.salir xd");
+		
+	}
+
+}
---
+++ src/claseObjeto.java
@@ -0,0 +1,51 @@
+
+public class claseObjeto {
+private double a;
+private double b;
+public claseObjeto(double a, double b) {
+	super();
+	this.a = a;
+	this.b = b;
+}
+public double getA() {
+	return a;
+}
+public void setA(double a) {
+	this.a = a;
+}
+public double getB() {
+	return b;
+}
+public void setB(double b) {
+	this.b = b;
+}
+public void suma(double a, double b) {
+	double suma;
+	suma=a+b;
+	System.out.println("La suma es "+suma);
+}
+public void resta(double a, double b) {
+	double resta;
+	resta=a-b;
+	System.out.println("La resta es: "+resta);
+	
+}
+public void multiplicacion(double a, double b) {
+	double multiplicacion;
+	multiplicacion=a*b;
+	System.out.println("La multiplicacion es: "+multiplicacion);
+}
+public void division(double a, double b) {
+	double division;
+	division=a/b;
+	System.out.println("La division es: "+division);
+}
+public void potenciacion(double a, double b) {
+	System.out.println("La potencia de estos números es: "+Math. pow(a,b));
+	Math. pow(a,b);
+}
+public void raiz(double a, double b) {
+	System.out.println("La raiz de a: "+Math.sqrt(a));
+	System.out.println("La raiz de b: "+Math.sqrt(b));
+}
+}
